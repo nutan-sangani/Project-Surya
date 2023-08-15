5402ed44dc8a02f0ab1b8fbd97ed058693fd4e0b
nutan <nutan.sangani.shri.ram@gmail.com>
2023-08-15 23:12:55 +0530

    add bookRequest page with pagination

--- server/routes/request.route.js
+++ server/routes/request.route.js
@@ -5,4 +5,6 @@ const {RequestController} = require('../controller');
 
 router.post('/book',auth(),RequestController.addRequest);
 
+router.get('/bookId',auth(),RequestController.requestForBookId);
+
 module.exports = router;
