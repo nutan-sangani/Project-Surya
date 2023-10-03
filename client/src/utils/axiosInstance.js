c98f9afaa26c57b7893e2f482c05ba1ceac9bab4
nutan <nutan.sangani.shri.ram@gmail.com>
2023-10-04 00:03:55 +0530

    changes to deploy frontend on vercel

--- client/src/utils/axiosInstance.js
+++ client/src/utils/axiosInstance.js
@@ -1,6 +1,8 @@
 import axios from "axios";
 
-const axiosInstance = axios.create();
+const axiosInstance = axios.create({
+    baseURL:process.env.REACT_APP_BACKEND_IP,
+});
 
 const token = localStorage.getItem('token');
 axiosInstance.defaults.headers.common["Authorization"] = token;
